2cd6e7a18ce4b96183f9370670867a6f799a4ea8
djgarcia007 <128425355+djgarcia007@users.noreply.github.com>
2023-11-17 09:39:18 -0700

    Delete deploytemplate.ps1

--- deploytemplate.ps1
+++
@@ -1,5 +0,0 @@
-# Login to Azure 
-Connect-AzAccount
-
-#- deploy template and parameters for storage account
-New-AzResourceGroupDeployment -ResourceGroupName 'team2' -TemplateFile 'D:\Users\dorien.garcia\source\repos\AzureChallengeTeam2\template.json' -TemplateParameterFile 'D:\Users\dorien.garcia\source\repos\AzureChallengeTeam2\templateparameters.json'
