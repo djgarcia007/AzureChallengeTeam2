1bd35a35d93a8f43548ea8632176c1bd99b2950f
djgarcia007 <128425355+djgarcia007@users.noreply.github.com>
2023-11-28 13:01:08 -0700

    Update script.js

--- script.js
+++ script.js
@@ -12,8 +12,6 @@ async function submit() {
     data['timeliness']=parseInt(document.querySelector('input[name="timeliness"]:checked').value);
     data['support']=parseInt(document.querySelector('input[name="support"]:checked').value);
     data['recommend']=parseInt(document.querySelector('input[name="recommend"]:checked').value);
-    //data=document.getElementById("comments").value
-    document.getElementById("comments").value=""
     try {
         const response=await fetch('https://prod-80.eastus.logic.azure.com:443/workflows/5a13e30726ea47f6809a8d9fb0e38237/triggers/manual/paths/invoke?api-version=2016-10-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=NNHHMAsYF0guTq1N6V4Mp5R40iavtwh6kFys-fy7Zts',
         {
@@ -41,14 +39,14 @@ async function submit() {
         updateAnalysis(err)
     }
 }
+
 function updateAnalysis(resData) {
     
     document.getElementById('analysis').innerHTML=Date.now()+" result: "+resData
     document.getElementById('analysis').innerHTML+='<br/>'+JSON.stringify(resData)
 }
 
-
-//Reuires API key to be set
+//Reqires API key to be set
 async function analyzeViaChatGPT() {
     /*curl https://api.openai.com/v1/chat/completions \
     -H "Content-Type: application/json" \
